68935d73272044072a28ca77ef327055e5d81525
Jordan Freundlich <75767764+Jfreundlich01@users.noreply.github.com>
2022-08-23 15:28:12 -0400

    added buttons

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 
 function App() {
   const counter = useSelector((state) => state.counter)
+
+  const increment = () =>{
+    
+  }
+  const decrement = () =>{
+    
+  }
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
 
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
       
     </div>
   );
