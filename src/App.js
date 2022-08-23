1318b998ffb586a11306c8fbaaf1c31e5e161e01
Jordan Freundlich <75767764+Jfreundlich01@users.noreply.github.com>
2022-08-23 16:13:43 -0400

    using redux toolkit now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { actions } from './store/index';
 import './App.css';
 
 function App() {
@@ -7,13 +8,13 @@ function App() {
   const counter = useSelector((state) => state.counter)
   const dispatch = useDispatch()
   const increment = () =>{
-    dispatch({type:'INC'})
+    dispatch(actions.increment())
   }
   const decrement = () =>{
-    dispatch({type:'DEC'})
+    dispatch(actions.decrement())
   }
   const addBy = () =>{
-    dispatch({type:'ADD',payload:input})
+    dispatch(actions.addBy(input))
   }
   const addInput = (e) =>{
     setInput(e.target.value)
--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,25 @@
-import {createStore} from 'redux';
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-
-const reducerFn = (state ={counter: 0}, action) =>{
-
-    //Synchronous Function
-    //We should not mutate the original state, should always be a copy. 
-
-
-    if(action.type === "INC"){
-        return {counter :state.counter + 1}
+const counterSlice = createSlice({
+    name:'counter',
+    initialState: {counter: 0},
+    reducers: {
+        increment(state,action){
+            state.counter ++;
+        },
+        decrement(state,action){
+            state.counter --
+        },
+        addBy(state,action){
+            state.counter += Number(action.payload)
+        }
     }
-    if(action.type === "DEC"){
-        return {counter :state.counter - 1}
-    }
-    if(action.type === "ADD"){
-        return {counter :state.counter + Number(action.payload)}
-    }
+})
 
+const store = configureStore({
+    reducer:counterSlice.reducer
+})
 
-    return state
-}
+export const actions = counterSlice.actions
+export default store;
 
-const store = createStore(reducerFn);
-export default store
